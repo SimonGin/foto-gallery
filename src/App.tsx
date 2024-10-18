3a47545b8a94bf52e3be6253385961996bbd512f
Simon Gin <tt26419@gmail.com>
2024-10-18 19:52:06 +0700

    Initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
